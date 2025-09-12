0d59b5a58a19bc892ca9e62696bf1fc75edf2909
Amos Bastian <amosbastian@gmail.com>
2025-09-12 15:24:11 +0200

    add reproduction for 'inferred type' error

---
+++ packages/worker-core/src/tool/my-tool.ts
@@ -0,0 +1,18 @@
+import { createTool } from "@mastra/core/tools";
+import { z } from "zod";
+
+export const myTool = createTool({
+  description: "My tool",
+  execute: async ({ context: { user } }) => {
+    return {
+      userId: user,
+    };
+  },
+  id: "Assign user",
+  inputSchema: z.object({
+    user: z.string().describe("User name"),
+  }),
+  outputSchema: z.object({
+    userId: z.string().describe("The ID of the user"),
+  }),
+});
