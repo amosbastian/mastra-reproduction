0dee6fb1be4f7b51c5b3a3950be78c587e8b324b
Amos Bastian <amosbastian@gmail.com>
2025-10-22 18:16:34 +0200

    chore: upgrade mastra deps

--- apps/worker/src/mastra/index.ts
+++ apps/worker/src/mastra/index.ts
@@ -1,9 +1,7 @@
 import { Mastra } from "@mastra/core/mastra";
 import "dotenv/config";
-import { db } from "@acme/db";
 import { myAgent } from "@acme/worker-core/agent";
 import { myWorkflow } from "@acme/worker-core/workflow";
-import cron from "node-cron";
 import { storage } from "./storage.js";
 
 export const mastra = new Mastra({
@@ -11,7 +9,7 @@ export const mastra = new Mastra({
     myAgent,
   },
   bundler: {
-    externals: ["execa"],
+    externals: ["execa", "@acme/db"],
   },
   storage,
   workflows: {
@@ -19,8 +17,8 @@ export const mastra = new Mastra({
   },
 });
 
-cron.schedule("* * * * *", async () => {
-  const users = await db.query.user.findMany();
+// cron.schedule("* * * * *", async () => {
+//   const users = await db.query.user.findMany();
 
-  console.log(users);
-});
+//   console.log(users);
+// });
